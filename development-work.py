f44b60505bd37551488fa34669f93085a49c0915
dayfinity <138693114+dayfinity@users.noreply.github.com>
2026-06-28 21:43:04 +0100

    Update development-work.py print(len(signed.raw_transaction.hex()))
    print(len(signed.raw_transaction.hex()))

--- development-work.py
+++ development-work.py
@@ -9,7 +9,21 @@ wallet = Account.from_key(KEY)
 
 tx = {
     "from": wallet.address,
+    "to": "0x0000000000000000000000000000000000000000",
+    "value": 0,
+    "gas": 115000,
+    "gasPrice": web3.to_wei(4, "gwei"),
+    "nonce": web3.eth.get_transaction_count(
+        wallet.address
+    ),
+    "chainId": 1,
+}
 
+signed = wallet.sign_transaction(tx)
+
+print("Key Metrics")
+print("traded tokens")
+print("blockchain networks")
 
 print(wallet.address)
 print(len(signed.raw_transaction.hex()))
